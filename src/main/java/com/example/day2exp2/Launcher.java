4b8be507fc313094b3cf6dfec6a3894637f080a2
theju12001 <tejukiccha12@gmail.com>
2026-03-10 12:40:06 +0530

    Deploying

---
+++ src/main/java/com/example/day2exp2/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.day2exp2;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
