2e0d0d759870324dcaeb0f4cdedea8a41f3f1b2c
theju12001 <tejukiccha12@gmail.com>
2026-03-10 14:49:49 +0530

    2nd deploy

--- src/main/java/com/example/day2exp2/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.day2exp2;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.day2exp2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.day2exp2 to javafx.fxml;
-    exports com.example.day2exp2;
-}
